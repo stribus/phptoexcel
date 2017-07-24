95e55aca167b0982dd84e5e3342873a79e7ce241
Flavio <Flavio@localhost>
2017-07-24 17:39:12 -0300

    carrega excel e transforma em ttabela html

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-		<?php
-			// put your code here
-		?>
+		<form enctype="multipart/form-data" action="upload.php" method="POST">
+			<!-- MAX_FILE_SIZE deve preceder o campo input -->
+<!--			<input type="hidden" name="MAX_FILE_SIZE" value="30000" />-->
+			<br>
+			<!-- O Nome do elemento input determina o nome da array $_FILES -->
+			<?php 
+				$a = array(
+						'xlsx'=>'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','xls'=>'application/vnd.ms-excel',					
+				);
+				
+				
+				?>
+			Enviar esse arquivo:<br>
+			<input name="userfile" type="file" accept="<?php  echo implode(',', $a); ?>" />
+			<br>
+			
+			<input type="submit" value="Enviar arquivo" />
     </body>
 </html>
