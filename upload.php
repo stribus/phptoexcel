4a9462538971dc1b7a8d0ee9f023717158860c90
Flavio <4411132+stribus@users.noreply.github.com>
2017-07-24 17:39:12 -0300

    carrega excel e transforma em ttabela html

---
+++ upload.php
@@ -0,0 +1,38 @@
+<?
+	require_once './Classes/PHPExcel/IOFactory.php';
+	$upload = $_FILES['userfile']['tmp_name'];
+
+
+	$objPHPExcel = PHPExcel_IOFactory::load($upload);
+	$a = $objPHPExcel->getActiveSheet()->toArray();
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+		<table border="1">
+			<?
+				if (is_array($a)) {
+					foreach ($a as $key => $linha) {
+						?>
+						<tr>
+							<?
+							foreach ($linha as $celula) {
+								echo "<td>$celula</td>";
+							}
+							?>
+						</tr>
+						<?
+					}
+				}
+			?>				
+
+		</table>
+
+
+	</body>
+</html>
